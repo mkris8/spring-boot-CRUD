530bf2c5523a20ae61f0663cebf01d895993c60a
manoj <manoj.krishnamurthy@gmail.com>
2024-07-24 10:28:54 +0100

    Added Lombok for getter setters and logging

--- src/main/java/com/api/employee/controller/EmployeeController.java
+++ src/main/java/com/api/employee/controller/EmployeeController.java
@@ -2,12 +2,14 @@ package com.api.employee.controller;
 
 import com.api.employee.entity.Employee;
 import com.api.employee.service.EmployeeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/employees")
 public class EmployeeController {
@@ -17,19 +19,19 @@ public class EmployeeController {
 
     @GetMapping("/all")
     public List<Employee> getAllEmployees() {
-        System.out.println("Getting all the employees .....");
+        log.info("Getting all the employees .....");
         return employeeService.findAllEmployees();
     }
 
     @GetMapping("/{id}")
     public Optional<Employee> getEmployeeById(@PathVariable Integer id) {
-        System.out.println("Getting employee by Id .....");
+        log.info("Getting employee by Id .....");
         return employeeService.findEmployeeById(id);
     }
 
     @PostMapping("/create")
     public Employee createEmployee(@RequestBody Employee employee){
-        System.out.println("Creating an employee .....");
+        log.info("Creating an employee .....");
         return employeeService.saveEmployee(employee);
     }
 
--- src/main/java/com/api/employee/entity/Employee.java
+++ src/main/java/com/api/employee/entity/Employee.java
@@ -3,26 +3,17 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 @Entity
 public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
